eb65bfd871052d3f4fcee4b3d64e946eed59cf29
Patrick Raynaud <par3@nrc.gov>
2020-11-04 13:21:11 -0500

    VS project and main program file
    Created a VS project and template main program file with 'hello world'

---
+++ miniFAVOR/miniFAVOR.f90
@@ -0,0 +1,25 @@
+!  miniFAVOR.f90 
+!
+!  FUNCTIONS:
+!  miniFAVOR - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: miniFAVOR
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program miniFAVOR
+
+    implicit none
+
+    ! Variables
+
+    ! Body of miniFAVOR
+    print *, 'Hello World'
+    
+    end program miniFAVOR
+
