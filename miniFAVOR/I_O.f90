b078e80b83296c34f4d8c74bb0006df293032b63
Patrick Raynaud <par3@nrc.gov>
2020-11-12 18:06:43 -0500

    MiniFAVOR except output
    main program and all subroutines and functions written.  Only output creation remains to be written.

--- miniFAVOR/I_O.f90
+++ miniFAVOR/I_O.f90
@@ -1,36 +1,71 @@
 module I_O
     
-    implicit none
+implicit none
     
     contains
-        subroutine read_IN(fn_IN, n_IN)
+    
+    subroutine read_IN(fn_IN, n_IN, n_ECHO)
             
-            use inputs_h
+        use inputs_h
             
-            !Variables
-            integer :: i, n_IN
-            character(len=64) :: fn_IN
+        !Variables
+        integer, intent(in) :: n_IN, n_ECHO
+        character(len=64), intent(in) :: fn_IN
+        character(len=64) :: fn_ECHO
+        integer :: i
             
-            !Open input file
-            open (unit=n_IN, file=fn_IN, status='old', form='formatted')
+        !Open input file and create echo file
+        open (unit=n_IN, file=fn_IN, status='old', form='formatted')
+        fn_ECHO = fn_IN(1:index(fn_IN, '.in')-1)//'.echo'
+        open (unit=n_ECHO, file=fn_ECHO, status='unknown', form='formatted')
             
-            !Read crack depth and vessel thickness
-            read (n_IN, '(2f10.3)') a, b
+        !Read and echo crack depth and vessel thickness
+        read (n_IN, *) a, b
+        write (n_ECHO, '(a25,f10.3,a)') 'Crack Depth: ', a, ' in'
+        write (n_ECHO, '(a25,f10.3,a)') 'Vessel Thickness: ', b, ' in'
             
-            !Read number of simulations to be performed andnumber of time steps
-            read (n_IN, '(2i10)') nsim, ntime
+        !Read and echo number of simulations to be performed andnumber of time steps
+        read (n_IN, *) nsim, ntime
+        write (n_ECHO, '(a25,i10)') 'Number of Simulations: ', nsim
+        write (n_ECHO, '(a25,i10)') 'Number of Time Steps: ', ntime
             
-            !Allocate stress and temperature arrays
-            allocate(stress(ntime))
-            allocate(temp(ntime))
+        !Read and echo embrittlement inputs
+        read (n_IN, *) Cu_ave, Ni_ave, Cu_sig, Ni_sig, fsurf, RTndt0
+        write (n_ECHO, '(a25,f10.3,a)') 'Copper Content: ', Cu_ave, ' %'
+        write (n_ECHO, '(a25,f10.3,a)') 'Nickel Content: ', Ni_ave, ' %'
+        write (n_ECHO, '(a25,f10.3,a)') 'Copper Content STDEV: ', Cu_sig, ' %'
+        write (n_ECHO, '(a25,f10.3,a)') 'Nickel Content STDEV: ', Ni_sig, ' %'
+        write (n_ECHO, '(a25,f10.3,a)') 'ID Surface Fluence: ', fsurf, ' n/cm^2'
+        write (n_ECHO, '(a25,f10.3,a)') 'Unirradiated RTndt: ', RTndt0, ' degF'
             
-            !Read stress and temerature inputs
-            read_transient: do  i = 1, ntime
-                read (n_IN, '(2f10.3)') stress(i), temp(i)
-            end do read_transient
+        !Allocate stress and temperature arrays
+        allocate(stress(ntime))
+        allocate(temp(ntime))
             
-            !Read embrittlement inputs
-            read (n_IN, '(4f10.3)') Cu, Ni, fsurf, RTndt0
+        !Read and echo stress and temerature inputs
+        write (n_ECHO, '(a)') 'Stress (ksi),      Temperature (degF)'
+        read_transient: do  i = 1, ntime
+            read (n_IN, *) stress(i), temp(i)
+            write (n_ECHO, '(f10.3,9x,f10.3)') stress(i), temp(i)
+        end do read_transient
             
-        end subroutine read_IN
+    end subroutine read_IN
+    
+    subroutine write_OUT(fn_IN, n_OUT)
+    
+        use outputs_h
+        
+        !Variables
+        character(len=64), intent(in) :: fn_IN
+        integer, intent(in) :: n_OUT
+        character(len=64) :: fn_OUT
+        integer :: i
+
+        !Open output file
+        fn_OUT = fn_IN(1:index(fn_IN, '.in')-1)//'.out'
+        open (unit=n_OUT, file=fn_OUT, status='unknown', form='formatted')
+        
+    end subroutine write_OUT
+    
+    
 end module I_O
--- miniFAVOR/miniFAVOR.f90
+++ miniFAVOR/miniFAVOR.f90
@@ -14,14 +14,20 @@
 
     program miniFAVOR
     
-    use I_O, only: read_IN
-    use inputs_h, only: nsim, ntime
+    use I_O
+    use inputs_h
+    use outputs_h
+    use calc_RTndt
+    use calc_K
+    use calc_cpi
 
     implicit none
 
     ! Variables
     character(len=64) :: fn_IN
     integer, parameter :: n_IN = 15
+    integer, parameter :: n_ECHO = n_IN + 1
+    integer, parameter :: n_OUT = n_IN + 2
     integer :: i, j
     
     ! Body of miniFAVOR
@@ -31,15 +37,51 @@
     read (*,'(a)') fn_IN
     
     !Read input file
-    call read_IN(fn_IN, n_IN)
+    call read_IN(fn_IN, n_IN, n_ECHO)
     
+    !Allocate output arrays
+    allocate(K_hist(ntime))
+    allocate(Chemistry(nsim,3))
+    allocate(cpi_hist(nsim, ntime))
+    allocate(CPI_results(nsim,3))
+    
+    !Initialize output arrays
+    K_hist =  0.0
+    Chemistry = 0.0
+    cpi_hist = 0.0
+    CPI_results = 0.0
+    
+    !Calculate applied stress intensity factor (SIF)
+    SIF_loop: do j = 1, ntime
+        K_hist(j) = Ki_t(a, b, stress(j))
+    end do SIF_loop
+        
     !Start looping over number of simulations
     Vessel_loop: do i = 1, nsim
+        
+        !Sample chemistry: Chemistry(i,1) is Cu content, Chemistry(i,2) is Ni content
+        call sample_chem(Cu_ave, Ni_ave, Cu_sig, Ni_sig, Chemistry(i,1), Chemistry(i,2))
+        
+        !Calculate chemistry factor: Chemistry(i,3) is chemistry factor
+        Chemistry(i,3) = CF(Chemistry(i,1), Chemistry(i,2))
+        
+        !Calculate RTndt for this vessel trial: CPI_results(i,1) is RTndt
+        CPI_results(i,1) = RTndt(a, Chemistry(i,3), fsurf, RTndt0)
+        
         !Start time loop
         Time_loop: do j = 1, ntime
-            !time dependent statements
+            !Calculate instantaneous cpi(t)
+            cpi_hist(i,j) = cpi_t(K_hist(j), CPI_results(i,1), temp(j))
         end do Time_loop
+        
+        !Calculate CPI for vessel 'i'
+        CPI_results(i,2) = maxval(cpi_hist(i,:))
+        
+        !Calculate moving average CPI for trials executed so far
+        CPI_results(i,3) = sum(CPI_results(:,2))/i
+                
     end do Vessel_loop
+    
     
     
     end program miniFAVOR
