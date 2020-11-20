093e987ad602568ba545fc4c2c0b193b5b1b7977
Brad Richardson <everythingfunctional@protonmail.com>
2020-11-20 09:34:30 -0600

    fix: Add random seed to make results repeatable

--- integration_test/run_test.sh
+++ integration_test/run_test.sh
@@ -7,5 +7,5 @@ rm -f test.dat test.echo test.out
 test.in
 EOF
 diff -b test.echo.orig test.echo
-numdiff -r 0.2 test.out.orig test.out
-numdiff -r 1.0 -a 1.0 test.dat.orig test.dat
+numdiff -r 1.0e-6 test.out.orig test.out
+numdiff -r 1.0e-6 test.dat.orig test.dat
