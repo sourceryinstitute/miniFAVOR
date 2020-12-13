c70052be87f3e516bf4e16882f165f386d7d7620
Damian Rouson <damian@sourceryinstitute.org>
2020-12-13 12:29:38 -0800

    refac(CF): associate immutable state; capture algo
    In addition to the new associations, this commit captures a
    repeated algorithm in the new rounded_and_bounded function.

--- src/Calc_RTndt.f90
+++ src/Calc_RTndt.f90
@@ -31,55 +31,64 @@ implicit none
         !Variables
         real :: CF
         real, intent(in) :: Cu, Ni
-        integer :: Cu_int, Ni_int
         real :: CF_1, CF_2
 
-        !Calculate indexes for copper interpolation:
-        !  multiply the Cu-% by 100 and take the integer truncation to find interpolation bounds
-        !  truncate interpolation between 0% and 0.40%
-        Cu_int = int(Cu*100)
-        if (Cu_int < 0) then
-            Cu_int = 0
-        else if (Cu_int > 40) then
-            Cu_int = 40
-        end if
+        ! Calculate interpolation coefficients for copper/nickel percentages:
+        ! Truncate copper/nickel percentages to ranges [0%, 0.40%] and [0%, 1.2%], respectively.
+        associate( &
+          Cu_int => rounded_and_bounded(Cu*100, bounds=[0, 40]), &
+          Ni_int => int(rounded_and_bounded(Ni*100, bounds=[0, 120])/20) + 1 &
+          ! Nickel contents in CF_weld are at intervals of 0.20% nickel
+        )
+          !Bi-linear interpolation
+          if (Cu <= 0.0 .or. Cu >= 0.40) then !only interpolate on nickel
+              select case (Ni_int)
+              case (7)
+                  CF = CF_weld(Cu_int, Ni_int)
+              case default
+                  CF = CF_weld(Cu_int,Ni_int) + &
+                  (Ni-0.2*(Ni_int-1))/0.2 * (CF_weld(Cu_int,Ni_int+1)-CF_weld(Cu_int,Ni_int))
+              end select
+          else
+              !First, interpolate on copper
+              select case (Ni_int)
+              case (7)
+                  CF = CF_weld(Cu_int, Ni_int) + &
+                  (Cu-0.01*(Cu_int))/0.01 * (CF_weld(Cu_int+1,Ni_int)-CF_weld(Cu_int+1,Ni_int))
+              case default
+                  CF_1 = CF_weld(Cu_int,Ni_int) + &
+                      (Cu-0.01*(Cu_int))/0.01 * (CF_weld(Cu_int+1,Ni_int)-CF_weld(Cu_int+1,Ni_int))
+                  CF_2 = CF_weld(Cu_int,Ni_int+1) + &
+                      (Cu-0.01*(Cu_int))/0.01 * (CF_weld(Cu_int+1,Ni_int+1)-CF_weld(Cu_int+1,Ni_int+1))
+              !Second, interpolate on nickel
+              CF = CF_1 + (Ni-0.2*(Ni_int-1))/0.2 * (CF_2-CF_1)
+              end select
+          end if
+        end associate
 
-        !Calculate indexes for nickel interpolation:
-        !  multiply the Ni-% by 100 and take the integer truncation to find interpolation bounds
-        !  truncate interpolation between 0% and 1.20%
-        Ni_int = int(Ni*100)
-        if (Ni_int < 0) then
-            Ni_int = 0
-        else if (Ni_int > 120) then
-            Ni_int = 120
-        end if
-        !Nickel contents in CF_weld are at intervals of 0.20% nickel
-        Ni_int = int(Ni_int/20)+1
+    contains
 
-        !Bi-linear interpolation
-        if (Cu <= 0.0 .or. Cu >= 0.40) then !only interpolate on nickel
-            select case (Ni_int)
-            case (7)
-                CF = CF_weld(Cu_int, Ni_int)
-            case default
-                CF = CF_weld(Cu_int,Ni_int) + &
-                (Ni-0.2*(Ni_int-1))/0.2 * (CF_weld(Cu_int,Ni_int+1)-CF_weld(Cu_int,Ni_int))
-            end select
-        else
-            !First, interpolate on copper
-            select case (Ni_int)
-            case (7)
-                CF = CF_weld(Cu_int, Ni_int) + &
-                (Cu-0.01*(Cu_int))/0.01 * (CF_weld(Cu_int+1,Ni_int)-CF_weld(Cu_int+1,Ni_int))
-            case default
-                CF_1 = CF_weld(Cu_int,Ni_int) + &
-                    (Cu-0.01*(Cu_int))/0.01 * (CF_weld(Cu_int+1,Ni_int)-CF_weld(Cu_int+1,Ni_int))
-                CF_2 = CF_weld(Cu_int,Ni_int+1) + &
-                    (Cu-0.01*(Cu_int))/0.01 * (CF_weld(Cu_int+1,Ni_int+1)-CF_weld(Cu_int+1,Ni_int+1))
-            !Second, interpolate on nickel
-            CF = CF_1 + (Ni-0.2*(Ni_int-1))/0.2 * (CF_2-CF_1)
-            end select
-        end if
+      pure function rounded_and_bounded(unbounded_value, bounds)
+        integer rounded_and_bounded
+        real, intent(in) :: unbounded_value
+        integer, intent(in) :: bounds(:)
+        integer, parameter :: end_points=2
+
+        call assert(size(bounds)==end_points, "Calc_RTndt|bounded_value: size(bounds)==end_points")
+
+        associate(floor_=>bounds(1), ceiling_=>bounds(2))
+
+          if (unbounded_value < floor_) then
+            rounded_and_bounded = floor_
+          else if (unbounded_value > ceiling_) then
+            rounded_and_bounded  = ceiling_
+          else
+            rounded_and_bounded = int(unbounded_value)
+          end if
+
+        end associate
+
+      end function
 
     end function CF
 
