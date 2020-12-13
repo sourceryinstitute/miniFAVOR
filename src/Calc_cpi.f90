60e7253a8934192436fece28aa188089a9ef7950
Damian Rouson <damian@sourceryinstitute.org>
2020-12-13 09:16:22 -0800

    refac(cpi_t): associater/parameter immutable state

--- src/Calc_cpi.f90
+++ src/Calc_cpi.f90
@@ -10,15 +10,16 @@ module calc_cpi
     !Variables
     real :: cpi_t
     real, intent(in) :: K, RTndt, T
-    real :: aKic, bKic, cKic
+    real, parameter :: cKic = 4.0
 
     !Calculate aKic, bKic, cKic
-    aKic = 19.35+8.335*exp(0.02254*(T-RTndt))
-    bKic = 15.61+50.132*exp(0.008*(T-RTndt))
-    cKic = 4.0
-
+    associate( &
+      aKic => 19.35+8.335*exp(0.02254*(T-RTndt)), &
+      bKic => 15.61+50.132*exp(0.008*(T-RTndt)) &
+    )
     !Calculate cpi_t
-    cpi_t = merge(0.0,1-exp(-((K-aKic)/bKic)**cKic), K < aKic)
+      cpi_t = merge(0.0,1-exp(-((K-aKic)/bKic)**cKic), K < aKic)
+    end associate
 
   end function cpi_t
 
