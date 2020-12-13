05ebe706a5d8b85fab613c4824130974df648cf6
Damian Rouson <damian@sourceryinstitute.org>
2020-12-13 09:17:47 -0800

    refac(cpi_t): eliminate superfluous comments

--- src/Calc_cpi.f90
+++ src/Calc_cpi.f90
@@ -4,20 +4,17 @@ module calc_cpi
 
  contains
 
-  !Function to calculate cpi(t)
+  ! calculate cpi(t)
   pure function cpi_t(K, RTndt, T)
 
-    !Variables
     real :: cpi_t
     real, intent(in) :: K, RTndt, T
     real, parameter :: cKic = 4.0
 
-    !Calculate aKic, bKic, cKic
     associate( &
       aKic => 19.35+8.335*exp(0.02254*(T-RTndt)), &
       bKic => 15.61+50.132*exp(0.008*(T-RTndt)) &
     )
-    !Calculate cpi_t
       cpi_t = merge(0.0,1-exp(-((K-aKic)/bKic)**cKic), K < aKic)
     end associate
 
