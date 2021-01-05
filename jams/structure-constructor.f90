0b7c414dc464193861849d5c4fc9f906fd262292
Damian Rouson <damian@sourceryinstitute.org>
2021-01-05 09:14:17 -0800

    demonstrate structure constructors

---
+++ jams/structure-constructor.f90
@@ -0,0 +1,23 @@
+module material_m
+  implicit none
+
+  private ! information hiding
+
+  type, public :: material_content_t ! encapsulate
+    real :: Ni=0.1, Cu=0.2
+  end type
+
+end module
+
+program main
+  use material_m, only : material_content_t
+  implicit none
+  type(material_content_t) vessel ! = material_content_t(0.3, 0.4)
+
+  real, parameter :: pi=3.141592654
+
+  print *, material_content_t()
+  print *, vessel
+  print *, vessel%Ni, vessel%Cu
+
+end program main
