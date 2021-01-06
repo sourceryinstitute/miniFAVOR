538878a8667a15d79cff435da2b2accf7a9930b5
Brad Richardson <everythingfunctional@protonmail.com>
2021-01-06 14:53:37 -0600

    test: add requirement that random samples actually have samples

--- jams/tdd_jams/test/random_sample_test.f90
+++ jams/tdd_jams/test/random_sample_test.f90
@@ -12,6 +12,7 @@ contains
         "random_sample_t", &
         [ it("is not marked as user_defined initially", check_not_user_defined) &
         , it("is marked as user_defined after calling define", check_user_defined) &
+        , it("once defined, has components between 0-1", check_components) &
         ])
   end function
 
@@ -36,5 +37,22 @@ contains
 
     call sample%define()
     result_ = assert_that(sample%user_defined())
+  end function
+
+  function check_components() result(result_)
+    use random_sample_m, only: random_sample_t
+    use vegetables, only: result_t, assert_that
+
+    type(result_t) :: result_
+
+    type(random_sample_t) :: sample
+
+    call sample%define()
+
+    result_ = &
+        assert_that(sample%Cu_sig_local() > 0.0 .and. sample%Cu_sig_local() < 1.0, "Cu_sig_local") &
+        .and.assert_that(sample%Cu_local() > 0.0 .and. sample%Cu_local() < 1.0, "Cu_local") &
+        .and.assert_that(sample%Ni_local() > 0.0 .and. sample%Ni_local() < 1.0, "Ni_local") &
+        .and.assert_that(sample%phi() > 0.0 .and. sample%phi() < 1.0, "phi")
   end function
 end module
