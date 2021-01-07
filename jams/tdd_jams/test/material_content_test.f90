15c5e98d3d7fb02e5a1a3767f1a03a0da2bb5921
Brad Richardson <everythingfunctional@protonmail.com>
2021-01-07 09:34:00 -0600

    fix(material_content_test): use the proper constructor

--- jams/tdd_jams/test/material_content_test.f90
+++ jams/tdd_jams/test/material_content_test.f90
@@ -1,5 +1,6 @@
 module material_content_test
   use material_content_m, only: material_content_t
+  use random_sample_m, only: random_sample_t
   use vegetables, only: test_item_t, describe, it, result_t, assert_that, assert_not
   implicit none
   private
@@ -13,7 +14,7 @@ contains
     tests = describe( &
         "material_content_t", &
         [ it("is not marked as user_defined initially", check_not_user_defined) &
-        , it("is marked as user_defined after calling define", check_user_defined) &
+        , it("is marked as user_defined after being constructed", check_user_defined) &
         ])
   end function
 
@@ -21,18 +22,21 @@ contains
 
     type(result_t) :: result_
 
-    type(material_content_t) :: sample
+    type(material_content_t) :: content
 
-    result_ = assert_not(sample%user_defined())
+    result_ = assert_not(content%user_defined())
   end function
 
   function check_user_defined() result(result_)
 
     type(result_t) :: result_
 
-    type(material_content_t) :: sample
+    type(material_content_t) :: content
+    type(random_sample_t) :: samples
 
-    call sample%define()
-    result_ = assert_that(sample%user_defined())
+    call samples%define()
+
+    content = material_content_t(0.1, 0.2, 0.01, 0.01, samples)
+    result_ = assert_that(content%user_defined())
   end function
 end module
